aa52697d0a7d78d0cef302ca7d31c0db13e3067f
yaraboualsood <yaramaher87@gmail.com>
2025-06-16 22:47:16 +0300

    fixing vercel deployment errors again

--- server/index.js
+++ server/index.js
@@ -19,7 +19,8 @@ const app = express();
 const allowedOrigins = [
     'http://localhost:5173',
     'http://localhost:3000',
-    'https://blogsy-pearl.vercel.app'
+    'https://blogsy-pearl.vercel.app',
+    'https://blogsy-yara-maher-abou-al-soods-projects.vercel.app'
 
 ];
 
